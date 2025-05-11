c26f2a71f4333c72ba7fd72b7a03b2905a8a49e3
0l3d <0l3dgit@gmail.com>
2025-05-11 13:23:19 +0300

    First

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,7 @@ shellLoop = do
   command <- getLine
   let n = 0
   when (n < 1) $ do
-    let n = if command == "exit" then 1 else 0
-    let commandList = words command
-    Shell.initShell commandList
+    Shell.initShell command
     shellLoop
 
 
--- src-lib/Shell.hs
+++ src-lib/Shell.hs
@@ -1,29 +1,44 @@
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
 module Shell (initShell, shellText) where
 
-import System.Exit ()
+import System.Exit (ExitCode (..), exitSuccess)
 import System.Directory (setCurrentDirectory, getCurrentDirectory)
 import System.Environment (getEnv)
 import System.Process
-import System.IO
-import GHC.Arr (safeIndex)
-import Text.Printf (printf)
+    ( createProcess,
+      proc,
+      waitForProcess,
+      CreateProcess(std_err, std_in, std_out),
+      StdStream(CreatePipe, Inherit), ProcessHandle )
+import System.IO ( stdout, hFlush, hGetContents, Handle )
+import Control.Exception ( SomeException, try )
 
 -- Shell initialize.
-initShell :: [String] -> IO ()
-initShell str = do
-  let (command:arguments) = str
-  if command == "cd"
-    then do
-      let directory =  if length arguments > 0 then Just (arguments !! 0) else Nothing
-      case directory of
-        Just path -> do
-          setCurrentDirectory path
-        Nothing -> putStrLn "no such file or directory"
+initShell :: String -> IO ()
+initShell command = do
+    if command == "exit" then do
+      putStrLn "Bye!"
+      exitSuccess
+    else if take 2 command == "cd" then do
+        let dir = drop 3 command
+        setCurrentDirectory dir
     else do
-      (Just hin, Just hout, Just herr, _) <- createProcess (proc command arguments)
-        { std_in = CreatePipe, std_out = CreatePipe, std_err = CreatePipe}
-      output <- hGetContents hout
-      putStrLn output
+        let args = words command
+        let cmd = head args
+        let cmdArgs = tail args
+
+        let processSpec = (proc cmd cmdArgs) { std_out = Inherit, std_err = Inherit, std_in = Inherit }
+
+        result <- try (createProcess processSpec) :: IO (Either SomeException (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle))
+        case result of
+            Left err -> do
+                putStrLn $ "Hata: " ++ show err
+                return ()
+            Right (_, _, _, pid) -> do
+                exitCode <- waitForProcess pid
+                case exitCode of
+                    ExitSuccess   -> return ()
+                    ExitFailure _ -> putStrLn "Command fail."
 
 shellText :: IO ()
 shellText = do
